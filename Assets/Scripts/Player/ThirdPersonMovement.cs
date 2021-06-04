e18fb5b3b7979ec4086ae7deb28609855e18cb44
MustachedNinja <kostya.yatsuk9@live.com>
2021-06-03 23:33:52 -0700

    Add multidirectional animator/blendtree

--- Assets/Scripts/Player/ThirdPersonMovement.cs
+++ Assets/Scripts/Player/ThirdPersonMovement.cs
@@ -4,10 +4,13 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     [SerializeField] private float _xSensitivity = 1000f;
     [SerializeField] private float _movementSpeed = 10f;
+
     private Rigidbody _rigidBody;
+    private Animator _animator;
 
     void Awake() {
         _rigidBody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
     void Update() {
@@ -18,10 +21,15 @@ public class ThirdPersonMovement : MonoBehaviour
     void FixedUpdate() {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            verticalInput *= 2.0f;
+        }
 
         Vector3 targetVelocity = new Vector3(horizontalInput, 0, verticalInput).normalized * _movementSpeed * Time.fixedDeltaTime;
         Vector3 offset = transform.rotation * targetVelocity;
 
+        _animator.SetFloat("Horizontal", horizontalInput, 0.1f, Time.fixedDeltaTime);
+        _animator.SetFloat("Vertical", verticalInput, 0.1f, Time.fixedDeltaTime);
         _rigidBody.MovePosition(transform.position + offset);
     }
 }
